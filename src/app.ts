c975e2acdd0e99701ff605001f5b03ca201eef71
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2025-07-27 10:52:43 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,18 @@ app.use(express.json({ limit: '50mb' })); // or larger if needed
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(cookieParser());
+// app.use(
+//   cors({
+//     // origin: true,
+//     origin: ['https://teerusn.com'],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   }),
+// );
+
 app.use(
   cors({
-    // origin: true,
-    origin: ['https://teerusn.com'],
+    origin: ['https://teerusn.com', 'https://www.teerusn.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }),
